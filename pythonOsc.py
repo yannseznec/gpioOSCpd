dfec5cce5a136e0a0e66d0dd159ab3b9cd9376e2
Yann Seznec <yannseznec@gmail.com>
2023-11-08 18:04:39 +0100

    Update pythonOsc.py

--- pythonOsc.py
+++ pythonOsc.py
@@ -1,8 +1,11 @@
 import os
+from gpiozero import Button
+button = Button(2)
 
 def send2Pd(message=''):
 	os.system("echo '" + message + "' | pdsend 3000")
 
+button.wait_for_press():
 def audioOn():
 	message = '0 1;'
 	send2Pd(message)
