e33f32df6695bdbccd135d7721db661451ea6491
Yann Seznec <yannseznec@gmail.com>
2023-11-08 18:09:12 +0100

    Update pythonOsc.py

--- pythonOsc.py
+++ pythonOsc.py
@@ -5,7 +5,7 @@ button = Button(2)
 def send2Pd(message=''):
 	os.system("echo '" + message + "' | pdsend 3000")
 
-button.wait_for_press():
+button.wait_for_press()
 def audioOn():
 	message = '0 1;'
 	send2Pd(message)
