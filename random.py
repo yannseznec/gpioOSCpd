9453a85f8ca006f80c1e66adaac5898e3f2dc3ae
Yann Seznec <yannseznec@gmail.com>
2023-11-08 18:17:07 +0100

    Create random.py

---
+++ random.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+OSC/Random Example: send random numbers to OSC.
+
+This example sends a pseudo-random number between 0 and 1024
+to the OSC receiver on UDP port 2222.
+"""
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
+import time
+import random
+
+def main():
+  oscSender = udp_client.UDPClient("localhost", 3000)
+  while True:
+    n = random.randint(0, 1024)
+    print(n)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/rand")
+    msg.add_arg(n)
+    oscSender.send(msg.build())
+
+    time.sleep(1)  
+
+if __name__ == "__main__":
+  main()
