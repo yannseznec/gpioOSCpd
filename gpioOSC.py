cfb225f773e2dfef2381d71450102c3e7278ab8a
Yann Seznec <yannseznec@gmail.com>
2023-11-09 18:18:12 +0100

    Update gpioOSC.py

--- gpioOSC.py
+++ gpioOSC.py
@@ -13,17 +13,16 @@ import RPi.GPIO as GPIO
 
 
 def main():
-    oscSender = udp_client.UDPClient("localhost", 2222)
-do_something()
+  oscSender = udp_client.UDPClient("localhost", 2222)
+  while True:
+    n = random.randint(0, 1024)
+  #  print(n)
 
+    msg = osc_message_builder.OscMessageBuilder(address = "/rand")
+    msg.add_arg(n)
+    oscSender.send(msg.build())
 
-    while True:
-        n = random.randint(0, 1024)
-        print(n)
-        msg = osc_message_builder.OscMessageBuilder(address = "/rand")
-        msg.add_arg(n)
-        oscSender.send(msg.build())
-        time.sleep(0.1)  
+    time.sleep(1)  
 
 if __name__ == "__main__":
   main()
