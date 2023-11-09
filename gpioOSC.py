3af60518bd9c9260de35b25912fe6408267c8f5f
Yann Seznec <yannseznec@gmail.com>
2023-11-09 18:20:02 +0100

    Update gpioOSC.py

--- gpioOSC.py
+++ gpioOSC.py
@@ -9,7 +9,13 @@ from pythonosc import udp_client
 from pythonosc import osc_message_builder
 import time
 import random
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+
 
 
 def main():
